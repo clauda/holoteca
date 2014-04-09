f3cdcc2147f5a556403c187a76995488bd841715
Claudia Farias <krawdyah@gmail.com>
2014-04-09 14:55:44 -0300

    damn redis

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-Holoteca::Application.config.session_store :redis_store
+Holoteca::Application.config.session_store :redis_store, redis_server: ENV['REDISTOGO_URL'], expires_in: 90.minutes
