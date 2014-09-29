fc58e1d1cd6e3e5da1dfa6ca31ae3d4dd62da59c
Claudia Farias <krawdyah@gmail.com>
2014-09-29 02:57:27 -0300

    fix sitemaps

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -3,7 +3,8 @@ require 'sitemap_generator'
 
 SitemapGenerator::Sitemap.default_host = 'http://holoteca.com.br'
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps'
-SitemapGenerator::Sitemap.create_index = false
+SitemapGenerator::Sitemap.compress = true
+SitemapGenerator::Sitemap.create_index = true
 SitemapGenerator::Sitemap.include_index = false
 SitemapGenerator::Sitemap.include_root = false
 
@@ -12,25 +13,22 @@ SitemapGenerator::Sitemap.create do
   add contact_path, changefreq: 'monthly'
   add privacy_path, changefreq: 'monthly'
 
-  Article.visible.each do |article|
-    add article_path(article), priority: 1.0, lastmod: article.updated_at
+  group(filename: :tags) do
+    Tag.all.each do |tag|
+      add tag_path(tag)
+    end
   end
-end
 
-Category.includes(:articles).all.each do |category|
-  SitemapGenerator::Sitemap.namer = SitemapGenerator::SimpleNamer.new category.to_param.to_sym
-  add category_path(category), changefreq: 'hourly', priority: 0.9
+  Category.includes(:articles).all.each do |category|
+    group(filename: category.slug_me.to_sym) do
+      add category_path(category), changefreq: 'hourly', priority: 0.9
 
-  SitemapGenerator::Sitemap.create do
-    category.articles.visible.each do |article|
-      add article_path(article), lastmod: article.published_at
+      category.articles.visible.each do |article|
+        add article_path(article), lastmod: article.published_at
+      end
     end
   end
 end
 
-Tag.includes(:articles).all.each do |tag|
-  SitemapGenerator::Sitemap.namer = SitemapGenerator::SimpleNamer.new tag.to_param.to_sym
-  add tag_path(tag), changefreq: 'hourly', priority: 0.9
-end
-
 SitemapGenerator::Sitemap.ping_search_engines
+
