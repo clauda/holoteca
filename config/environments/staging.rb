414f9db8b0b663f5d2df4e80692cbce6734469ef
Claudia Farias <krawdyah@gmail.com>
2015-07-26 15:25:20 -0300

    fix staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -80,7 +80,11 @@ Holoteca::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  
+
+  config.action_controller.asset_host = 'staging.holoteca.com.br'
+  config.action_mailer.asset_host = 'http://staging.holoteca.com.br'
+  config.action_mailer.default_url_options = { host: 'staging.holoteca.com.br' }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { 
     address: "smtp.mandrillapp.com", 
